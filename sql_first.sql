673fbee7114af4bb5ac05916024b4670ed093774
Enni <38839040+Ennix12345@users.noreply.github.com>
2021-08-09 18:53:03 +0300

    Delete sql_first.sql

--- sql_first.sql
+++
@@ -1,6 +0,0 @@
-SELECT * FROM mastermix.shop;
-update shop set price=13.4 where article=0001;
-alter table shop add column dee double(10,0);
-update shop set ppk=10.13 where article=0003;
-insert into shop (article, dealer, price, set_shop, ppk, dee) values(0005, 'HV', 45.77, null, 55.90, 42.18);
-update shop set price=10.13 where article=0001;
