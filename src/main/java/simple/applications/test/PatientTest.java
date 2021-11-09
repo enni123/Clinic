44b394d1a91bc26ba37fb8c14c5bd74c9d9a2125
Ennix12345 <ennix123@gmail.com>
2021-11-09 16:25:18 +0200

    change html file

--- src/main/java/simple/applications/test/PatientTest.java
+++ src/main/java/simple/applications/test/PatientTest.java
@@ -26,6 +26,7 @@ public class PatientTest {
         Patient name = repository.getPatientByName("Денис");
         Assertions.assertThat(name).hasFieldOrProperty("name");
         Assertions.assertThat(name.getEmail()).isEqualTo("denis33@ukr.net");
+
     }
     @Test
     public void findPatient(){
